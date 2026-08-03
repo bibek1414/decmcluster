45d29da1cbfb30f8a88cdc116a6e8374ff097138
bibek1414 <bbhattarai770@gmail.com>
2026-08-03 11:56:03 +0545

    build: configure Next.js output to standalone mode for deployment optimization

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname),
   /* config options here */
   allowedDevOrigins: ["192.168.1.80"],
   images: {
